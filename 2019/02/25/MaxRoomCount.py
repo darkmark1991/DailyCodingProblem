a2a57bbab2a4be1baa950a4597a3adf2f0517dfa
Amiran Ramishvili <amiran.ramishvili@gmail.com>
2019-02-26 14:28:02 +0400

    [21] added implementation that doesn't suck

--- 2019/02/25/MaxRoomCount.py
+++ 2019/02/25/MaxRoomCount.py
@@ -2,7 +2,7 @@
 
 import sys
 
-def MinRoomCount (arr):
+def MinRoomCountThatSucks (arr):
     maxCount = 0
     start = sys.maxsize
     end = 0
@@ -20,10 +20,36 @@ def MinRoomCount (arr):
     
     return maxCount
     
+def MinRoomCount (arr):
+    maxCount = 0
+    length = len(arr)
+    i = 0
+    
+    while (i < length):
+        j = 0
+        count = 1
+        while (j < length):
+            if j != i and arr[j][0] < arr[i][0] < arr[j][1]:
+                count += 1
+            j += 1
+        i += 1
+        maxCount = count if count > maxCount else maxCount
+    
+    return maxCount
+    
 
 times = [(30, 75), (0, 50), (60, 150)]
-count = MinRoomCount(times)
+count = MinRoomCount2(times)
 print("For the following schedule: ", times)
 print("Minimum number of rooms required is: ", count)
 # expected result: 2
 
+# time benchmarks below
+"""
+if __name__ == "__main__":
+    import timeit
+    setup = "from __main__ import MinRoomCount"
+    print("MinRoomCount: ", timeit.timeit("MinRoomCount([(30, 75), (0, 50), (60, 150)])", setup=setup))
+    setup = "from __main__ import MinRoomCountThatSucks"
+    print("MinRoomCountThatSucks: ", timeit.timeit("MinRoomCountThatSucks([(30, 75), (0, 50), (60, 150)])", setup=setup))
+"""
