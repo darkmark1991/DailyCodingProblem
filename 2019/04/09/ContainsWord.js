2933ea2ca05799ce5fa80c0d1ca25206bd3403b1
Amiran Ramishvili <amiran.ramishvili@gmail.com>
2019-04-10 18:21:38 +0400

    [63] solution in js cleanup

--- 2019/04/09/ContainsWord.js
+++ 2019/04/09/ContainsWord.js
@@ -2,23 +2,22 @@
 
 const ContainsWord = (matrix, word) => {
     const height = matrix.length;
-  const width = matrix[0].length;
+    const width = matrix[0].length;
 
     for (let i = 0; i < height; i++) {
-      for (let j = 0; j < width; j++) {
-          if (matrix[i][j] === word[0]) {
-              if (matrix[i].join('').includes(word)) return true;
-                let tmp = word[0];
-                for (let k = i+1; k < height; k++) {
-                  tmp = `${tmp}${matrix[k][j]}`;
-                    if (tmp === word) return true;
-              }
-          }
-      }
-  }
-return false;
+        for (let j = 0; j < width; j++) {
+            if (matrix[i][j] === word[0]) {
+                if (matrix[i].join('').includes(word)) return true;
+                    let tmp = word[0];
+                    for (let k = i+1; k < height; k++) {
+                    tmp = `${tmp}${matrix[k][j]}`;
+                        if (tmp === word) return true;
+                }
+            }
+        }
+    }
+    return false;
 }
-
 
 const matrix = [['F', 'A', 'C', 'I'],
               ['O', 'B', 'Q', 'P'],
@@ -28,6 +27,6 @@ const matrix = [['F', 'A', 'C', 'I'],
 const testWord = "FOAM";
 
 if (ContainsWord(matrix, testWord))
-console.log(`The given matrix contains the test word: ${testWord}`);
+    console.log(`The given matrix contains the test word: ${testWord}`);
 else
-console.log(`The given matrix does not contain the test word: ${testWord}`);
+    console.log(`The given matrix does not contain the test word: ${testWord}`);
